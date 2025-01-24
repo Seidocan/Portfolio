86d54228db40f50341bcc44d1995140eee73a111
Marco Marrocu <marco.marrocu@hotmail.de>
2025-01-24 19:58:19 +0100

    first commit

---
+++ src/app/myprojects/myprojects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myprojects',
+  standalone: true,
+  imports: [],
+  templateUrl: './myprojects.component.html',
+  styleUrl: './myprojects.component.scss'
+})
+export class MyprojectsComponent {
+
+}
