f1f3318a6e4063c0c672f5c933b278de2477b72a
Marco Marrocu <marco.marrocu@hotmail.de>
2025-02-11 12:57:38 +0100

    reworked some animations

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MenueComponent } from './menu.component';
+import { MenuComponent } from './menu.component';
 
-describe('MenueComponent', () => {
-  let component: MenueComponent;
-  let fixture: ComponentFixture<MenueComponent>;
+describe('MenuComponent', () => {
+  let component: MenuComponent;
+  let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MenueComponent]
+      imports: [MenuComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(MenueComponent);
+    fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
